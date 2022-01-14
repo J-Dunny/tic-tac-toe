c8ebc99b1df2b345a33d5f69cd0754d642aa5267
Jake Dunafon <jlad795@gmail.com>
2022-01-14 12:56:44 -0800

    Add Player/play function

--- player.js
+++ player.js
@@ -5,7 +5,58 @@ class Player {
     this.wins = 0;
   }
 
-  
+  play(e){
+    for (var i in newGame.board){
+      if (e.target.id === i){
+        newGame.board[i] = this.id
+      }
+    }
+// --------- WIN ------------------------------------
+    if ((newGame.board[0] === this.id && newGame.board[1] === this.id && newGame.board[2] === this.id)
+    || (newGame.board[3] === this.id && newGame.board[4] === this.id && newGame.board[5] === this.id)
+    || (newGame.board[6] === this.id && newGame.board[7] === this.id && newGame.board[8] === this.id)
+    || (newGame.board[0] === this.id && newGame.board[3] === this.id && newGame.board[6] === this.id)
+    || (newGame.board[1] === this.id && newGame.board[4] === this.id && newGame.board[7] === this.id)
+    || (newGame.board[2] === this.id && newGame.board[5] === this.id && newGame.board[8] === this.id)
+    || (newGame.board[2] === this.id && newGame.board[4] === this.id && newGame.board[6] === this.id)
+    || (newGame.board[0] === this.id && newGame.board[4] === this.id && newGame.board[8] === this.id)){
+      console.log("winner")
+
+      this.wins += 1;
+      win()
+      console.log(this.wins)
+      setTimeout('resetBoard()', 1500)
+    }
+// DRAW ----------------------------------
+    for (var i in newGame.board){
+      if (!newGame.board.includes(0)){
+        console.log("draw")
+        setTimeout('resetBoard()', 1500)
+      }
+    }
+  }
+
+
+
+//   winner(){
+//     if ((newGame.board[0] === this.id && newGame.board[1] === this.id && newGame.board[2] === this.id)
+//     || (newGame.board[3] === this.id && newGame.board[4] === this.id && newGame.board[5] === this.id)
+//     || (newGame.board[6] === this.id && newGame.board[7] === this.id && newGame.board[8] === this.id)
+//     || (newGame.board[0] === this.id && newGame.board[3] === this.id && newGame.board[6] === this.id)
+//     || (newGame.board[1] === this.id && newGame.board[4] === this.id && newGame.board[7] === this.id)
+//     || (newGame.board[2] === this.id && newGame.board[5] === this.id && newGame.board[8] === this.id)
+//     || (newGame.board[2] === this.id && newGame.board[4] === this.id && newGame.board[6] === this.id)
+//     || (newGame.board[0] === this.id && newGame.board[4] === this.id && newGame.board[8] === this.id)){
+//       console.log("winner")
+//
+//       this.wins += 1;
+//       // win()
+//       console.log(this.wins)
+//       setTimeout('resetBoard()', 1500)
+//   }
+//
+//
+// }
 }
 // setTimeout('resetBoard()', 2000)
 
