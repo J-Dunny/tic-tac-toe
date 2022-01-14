35cc444120f25370fbecb096c07a3c0cfe39c783
Jake Dunafon <jlad795@gmail.com>
2022-01-14 12:48:43 -0800

    Add game class

--- game.js
+++ game.js
@@ -7,54 +7,6 @@ class Game {
     this.player1 = new Player(1, '🔥')
     this.player2 = new Player(2, '❄️')
   }
-// '<img src="./assets/ice.jpeg" width="40px" alt="">'
-  reset() {
-    this.board = [0,0,0,
-                  0,0,0,
-                  0,0,0]
-  }
-
-  makeMove(e) {
-      if (!this.whosTurn ) {
-        newGame.player1.play(e)
-        this.whosTurn += 1;
-        console.log(e.target.id)
-        console.log("player1")
-      }
-      else if (this.whosTurn)  {
-        newGame.player2.play(e)
-        this.whosTurn = 0;
-        console.log("player2")
-        console.log(e.target.id)
-      }
-  // console.log(newGame.board)
-
-    }
-
-
-    // play(e, player){
-    //   for (var i in this.board)
-    //     if (e.target.id === i){
-    //       this.board[i] = this.id
-    //     }
-    //
-    //   if ((this.board[0] === player.id && this.board[1] === player.id && this.board[2] === player.id)
-    //   || (this.board[3] === player.id && this.board[4] === player.id && this.board[5] === player.id)
-    //   || (this.board[6] === player.id && this.board[7] === player.id && this.board[8] === player.id)
-    //   || (this.board[0] === player.id && this.board[3] === player.id && this.board[6] === player.id)
-    //   || (this.board[1] === player.id && this.board[4] === player.id && this.board[7] === player.id)
-    //   || (this.board[2] === player.id && this.board[5] === player.id && this.board[8] === player.id)
-    //   || (this.board[2] === player.id && this.board[4] === player.id && this.board[6] === player.id)
-    //   || (this.board[0] === player.id && this.board[4] === player.id && this.board[8] === player.id)){
-    //     console.log("winner")
-    //
-    //     player.wins += 1;
-    //     console.log("player wins")
-    //     console.log(player.wins)
-    //     setTimeout('resetBoard()', 1500)
-    //   }
-    // }
-
 
 }
 
