98654d81de68c71a8a4b898afd6b69b943d57044
Jake Dunafon <jlad795@gmail.com>
2022-01-14 20:05:34 -0800

    Fix win display

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var gameGrid = document.querySelector(".grid-section");
 var whosTurn = document.querySelector(".whos-turn");
 var playerTurn = document.querySelector("#playerTurn");
 var player1Turn = document.querySelector(".player1-turn");
+var player2Turn = document.querySelector(".player2-turn");
 var boxes = document.querySelectorAll(".box");
 var player1Wins = document.querySelector(".player-1-wins");
 var player2Wins = document.querySelector(".player-2-wins");
@@ -46,24 +47,25 @@ function displayReset(){
       boxes[i].innerHTML = '';
     }
     winnerDraw.innerText = ``;
+    whosTurn.innerHTML = `<h3 id="playerTurn" class="player1-turn">It's ${newGame.player1.token}'s turn </h3>`
   }
 
 function displayWin(){
   if (newGame.win(newGame.player1)){
     player1Wins.innerText = `Wins: ${newGame.player1.wins}`;
-    winnerDraw.innerText = `Player ${newGame.player1.token} wins!`
-    setTimeout('displayReset()', 1500)
+    whosTurn.innerHTML  = `<h3 id="playerTurn" class="player1-turn"> ${newGame.player1.token}  WINS! </h3>`
+    setTimeout('displayReset()', 2000)
 
   } else if (newGame.win(newGame.player2)){
     player2Wins.innerText = `Wins: ${newGame.player2.wins}`;
-    winnerDraw.innerText = `Player ${newGame.player2.token} wins!`
-    setTimeout('displayReset()', 1500)
+    whosTurn.innerHTML  = `<h3 id="playerTurn" class="player1-turn"> ${newGame.player2.token}  WINS! </h3>`
+    setTimeout('displayReset()', 2000)
   }
 }
 
 function displayDraw(){
   if (newGame.draw()){
-    winnerDraw.innerText = `It's a cats game`
+    whosTurn.innerHTML = `<h3 id="playerTurn" class="cats-game">It's a cat's game</h3>`
     setTimeout('displayReset()', 1500)
   }
 }
